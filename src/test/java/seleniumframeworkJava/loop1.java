e6aa7355092ff13d1bcc3432d9d2f29e743c44ff
unknown <rajkumar.yadav@franconnect.com>
2022-04-27 00:23:46 +0530

    morning class changes

---
+++ src/test/java/seleniumframeworkJava/loop1.java
@@ -0,0 +1,57 @@
+package seleniumframeworkJava;
+
+import org.testng.annotations.Test;
+
+public class loop1 {	
+	
+	
+
+	public void whileloop()
+	{
+		int x=1;
+		while (x<=4)
+		{
+			System.out.println("whileloop ************" +x);
+			//x=x+4;
+		//	x++;
+			x=x+1;
+					
+		}		
+	}
+	
+	
+	
+	
+	
+
+	
+	
+	public void forloop()
+	{
+		for (int x=1;x<=4;x++)
+			
+			
+			System.out.println("forloop**********" + x);
+	}	
+
+	@Test
+	public void ifandelse()
+	{
+		int x=2;
+		int y=4;
+		
+		if(x>y)
+		{
+			System.out.println("x is greater then y");
+			
+		}
+		else 
+		{
+			System.out.println("x is less then y");
+		}
+		
+		
+	}
+
+
+}
---
+++ src/test/java/seleniumframeworkJava/loop2.java
@@ -0,0 +1,13 @@
+package seleniumframeworkJava;
+
+public class loop2 {
+	
+	
+	
+	
+
+	public static void main(String[] args) {
+
+	}
+
+}
